b1a22ceafce0ddb8696f0f8612b08e8094b3718d
NavodContrath <navod.contrath@gmail.com>
2025-02-13 14:56:33 +0100

    add prompt

--- bonus/main.js
+++ bonus/main.js
@@ -4,3 +4,4 @@ Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche
 Nota: Non è necessario provvedere alla validazione delle email */
 
 const mailList = ["yacufojeddo-5843@yopmail.com", "citefeubosu-3756@yopmail.com", "xinocrarauje-2934@yopmail.com", "pixiprappalu-2459@yopmail.com", "dimmelo12@gmail.com"]
+const userMail = prompt("Type your e-mail")
