5ad26f1971ea16d30ac72c9dc353c3b16a8b8622
NavodContrath <navod.contrath@gmail.com>
2025-02-13 14:52:59 +0100

    add IF with results

--- dadi/main.js
+++ dadi/main.js
@@ -3,9 +3,18 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. St
 
 /*SUMMARY
 - math random for user and computer numbers
+- log the FIXED numbers 
 - IF to check who won (larger number wins)
 */
 
 //- math random for user and computer numbers
 const userNumber = Math.random() * 100
 const computerNumber = Math.random() * 100
+//- log the FIXED numbers 
+console.log(userNumber.toFixed(), computerNumber.toFixed());
+//- IF to check who won (larger number wins)
+if (userNumber > computerNumber) {
+    console.log("user wins!");
+} else {
+    console.log("computer wins!");
+}
