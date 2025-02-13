da8fc2e5b7ff5263b2c2dc33f590e7bbad034626
NavodContrath <navod.contrath@gmail.com>
2025-02-13 14:56:03 +0100

    add maillist

--- bonus/main.js
+++ bonus/main.js
@@ -3,3 +3,4 @@ Bonus:
 Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
 Nota: Non è necessario provvedere alla validazione delle email */
 
+const mailList = ["yacufojeddo-5843@yopmail.com", "citefeubosu-3756@yopmail.com", "xinocrarauje-2934@yopmail.com", "pixiprappalu-2459@yopmail.com", "dimmelo12@gmail.com"]
