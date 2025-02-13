0cb416b0b52322f2233f758a176a914e6b91bb34
NavodContrath <navod.contrath@gmail.com>
2025-02-13 14:55:42 +0100

    bonus

---
+++ bonus/main.js
@@ -0,0 +1,5 @@
+/* 
+Bonus:
+Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
+Nota: Non è necessario provvedere alla validazione delle email */
+
